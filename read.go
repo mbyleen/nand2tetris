91ad6a2c7e02aae448125ff3c7e1ec0aedd6c799
Marie Byleen <6962330+mbyleen@users.noreply.github.com>
2018-01-05 10:12:38 -0600

    Read an input file in line by line and store in memory

---
+++ read.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+func read(filename string) ([]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, nil
+}
