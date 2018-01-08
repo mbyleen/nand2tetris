ed71a4cc086cac3c87e2e3d6e843409c06231edc
Marie Byleen <6962330+mbyleen@users.noreply.github.com>
2018-01-08 10:35:01 -0600

    Parse C instructions

--- parse.go
+++ parse.go
@@ -13,7 +13,11 @@ func parse(input []string) ([]string, error) {
 		// identify A instructions
 		if strings.HasPrefix(line, "@") {
 			b = parseAInstruction(line)
+		} else {
+			// Non-A instructions are C instructions
+			b = parseCInstruction(line)
 		}
+		// remove this empty string check when working with symbols
 		if b != "" {
 			binary = append(binary, b)
 		}
@@ -25,7 +29,7 @@ func parse(input []string) ([]string, error) {
 // currently returns an empty string for non-numerical instructions
 // will need to change this when work with symbols later
 func parseAInstruction(s string) string {
-	//strip the prefix
+	//strip the @ prefix
 	s = s[1:]
 	//see if the remainder will parse as a number
 	n, err := strconv.ParseInt(s, 0, 15)
@@ -34,4 +38,33 @@ func parseAInstruction(s string) string {
 	}
 	//convert the number to binary
 	return fmt.Sprintf("%015b", n)
+}
+
+// Parse C instruction using lookup tables
+// 1 1 1 a c1 c2 c3 c4 c5 c6 d1 d2 d3 j1 j2 j3
+// dest=comp;jump
+func parseCInstruction(s string) string {
+	ins := "111"
+	var dest string
+	var comp string
+	var jump string
+
+	first := strings.Split(s, "=")
+	if len(first) > 1 {
+		dest = getDest(first[0])
+		ind := strings.Index(s, "=")
+		s = s[ind+1:]
+	} else {
+		dest = getDest("null")
+	}
+
+	second := strings.Split(s, ";")
+	comp = getComp(second[0])
+	if len(second) > 1 {
+		jump = getJump(second[1])
+	} else {
+		jump = getJump("null")
+	}
+
+	return ins + comp + dest + jump
 }
--- parse_test.go
+++ parse_test.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"strings"
 	"testing"
 )
 
 func TestAinstructionNumbers(t *testing.T) {
-	//what are the pos and neg numbers of greatest magnitude expected?
+	// What is the number of greatest magnitude expected?
 	//negative numbers are not legal--can expect a valid input for this exercise
 	input := []string{"@84", "@0", "@notNumber", "notAType"}
 	// parse the test file
@@ -17,10 +16,23 @@ func TestAinstructionNumbers(t *testing.T) {
 	}
 	// test against expected output
 	expected := []string{"000000001010100", "000000000000000"}
-	for i, _ := range output {
-		fmt.Println(output[i], expected[i])
-
+	for i, _ := range expected {
 		if strings.Compare(output[i], expected[i]) != 0 {
+			t.Error("Match failed:", output[i], expected[i])
+		}
+	}
+}
+
+func TestCinstruction(t *testing.T) {
+	// limited test cases--will test looku tables with longer examples
+	testCases := []string{"A=D+1", "0;JMP", "D;JNE"}
+	expected := []string{"1110011111100000", "1110101010000111", "1110001100000101"}
+	output, err := parse(testCases)
+	if err != nil {
+		t.Error()
+	}
+	for i, _ := range expected {
+		if strings.Compare(expected[i], output[i]) != 0 {
 			t.Error("Match failed:", output[i], expected[i])
 		}
 	}
